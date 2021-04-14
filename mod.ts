90aa8785ecd7346a353c975d3c6dd6fa6255c358
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2021-04-14 00:09:43 -0700

    Adding the Stats slash command

--- mod.ts
+++ mod.ts
@@ -1,98 +1,6 @@
 // Sift is a small routing library that abstracts the details like registering
 // a fetch event listener and provides a simple function (serve) that has an
-// API to i// Sift is a small routing library that abstracts the details like registering
-// // a fetch event listener and provides a simple function (serve) that has an
-// // API to invoke a funciton for a specific path.
-// import {
-//     json,
-//     serve,
-//     validateRequest,
-// } from "https://deno.land/x/sift@0.1.7/mod.ts";
-// // TweetNaCl is a cryptography library that we use to verify requests
-// // from Discord.
-// import nacl from "https://cdn.skypack.dev/tweetnacl@v1.0.3";
-//
-// // For all requests to "/" endpoint, we want to invoke home() handler.
-// serve({
-//     "/": home,
-// });
-//
-// // The main logic of the Discord Slash Command is defined in this function.
-// async function home(request: Request) {
-//     // validateRequest() ensures that a request is of POST method and
-//     // has the following headers.
-//     const { error } = await validateRequest(request, {
-//         POST: {
-//             headers: ["X-Signature-Ed25519", "X-Signature-Timestamp"],
-//         },
-//     });
-//     if (error) {
-//         return json({ error: error.message }, { status: error.status });
-//     }
-//
-//     // verifySignature() verifies if the request is coming from Discord.
-//     // When the request's signature is not valid, we return a 401 and this is
-//     // important as Discord sends invalid requests to test our verification.
-//     const { valid, body } = await verifySignature(request);
-//     if (!valid) {
-//         return json(
-//             { error: "Invalid request" },
-//             {
-//                 status: 401,
-//             },
-//         );
-//     }
-//
-//     const { type = 0, data = { options: [] } } = JSON.parse(body);
-//     // Discord performs Ping interactions to test our application.
-//     // Type 1 in a request implies a Ping interaction.
-//     if (type === 1) {
-//         return json({
-//             type: 1, // Type 1 in a response is a Pong interaction response type.
-//         });
-//     }
-//
-//     // Type 2 in a request is an ApplicationCommand interaction.
-//     // It implies that a user has issued a command.
-//     if (type === 2) {
-//         const { value } = data.options.find((option) => option.name === "name");
-//         return json({
-//             // Type 4 reponds with the below message retaining the user's
-//             // input at the top.
-//             type: 4,
-//             data: {
-//                 content: `Hello, ${value}!`,
-//             },
-//         });
-//     }
-//
-//     // We will return a bad request error as a valid Discord request
-//     // shouldn't reach here.
-//     return json({ error: "bad request" }, { status: 400 });
-// }
-//
-// /** Verify whether the request is coming from Discord. */
-// async function verifySignature(
-//     request: Request,
-// ): Promise<{ valid: boolean; body: string }> {
-//     const PUBLIC_KEY = Deno.env.get("DISCORD_PUBLIC_KEY")!;
-//     // Discord sends these headers with every request.
-//     const signature = request.headers.get("X-Signature-Ed25519")!;
-//     const timestamp = request.headers.get("X-Signature-Timestamp")!;
-//     const body = await request.text();
-//     const valid = nacl.sign.detached.verify(
-//         new TextEncoder().encode(timestamp + body),
-//         hexToUint8Array(signature),
-//         hexToUint8Array(PUBLIC_KEY),
-//     );
-//
-//     return { valid, body };
-// }
-//
-// /** Converts a hexadecimal string to Uint8Array. */
-// function hexToUint8Array(hex: string) {
-//     return new Uint8Array(hex.match(/.{1,2}/g)!.map((val) => parseInt(val, 16)));
-// }nvoke a funciton for a specific path.
+// API to invoke a funciton for a specific path.
 import {
     json,
     serve,
@@ -109,6 +17,7 @@ serve({
 
 // The main logic of the Discord Slash Command is defined in this function.
 async function home(request: Request) {
+    console.debug(request);
     // validateRequest() ensures that a request is of POST method and
     // has the following headers.
     const { error } = await validateRequest(request, {
@@ -145,16 +54,23 @@ async function home(request: Request) {
     // Type 2 in a request is an ApplicationCommand interaction.
     // It implies that a user has issued a command.
     if (type === 2) {
-        const { value } = data.options.find((option: { name: string; }) => option.name === "name");
-        console.log(value);
-        return json({
-            // Type 4 reponds with the below message retaining the user's
-            // input at the top.
-            type: 4,
-            data: {
-                content: `Hello, ${value}!`,
-            },
-        });
+        const { name, value } = data.options.find((option: { name: string; }) => option.name === "name");
+        if(name === "hello")
+            return json({
+                // Type 4 reponds with the below message retaining the user's
+                // input at the top.
+                type: 4,
+                data: {
+                    content: `Hello, ${value}!`,
+                },
+            });
+        else if(name === "stats")
+            return json({
+                type: 4,
+                data: {
+                    content: `Stats!`
+                }
+            });
     }
 
     // We will return a bad request error as a valid Discord request
@@ -184,3 +100,12 @@ async function verifySignature(
 function hexToUint8Array(hex: string) {
     return new Uint8Array(hex.match(/.{1,2}/g)!.map((val) => parseInt(val, 16)));
 }
+
+
+/**
+ * curl -X POST \
+ -H 'Content-Type: application/json' \
+ -H "Authorization: Bot $BOT_TOKEN" \
+ -d '{"name":"hello","description":"Greet a person","options":[{"name":"name","description":"The name of the person","type":3,"required":true}]}' \
+ "https://discord.com/api/v8/applications/$CLIENT_ID/commands"
+ */
